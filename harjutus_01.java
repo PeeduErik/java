3ec134156c60be05cf98795a30752d3778d10b9b
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2022-09-07 09:43:27 +0300

    Add files via upload

---
+++ harjutus_01.java
@@ -0,0 +1,22 @@
+package ül;
+
+public class harjutus_01 {
+
+
+ public static void main(String[] args) {
+ String eesnimi = new String();
+ String kursus = new String();
+ String huud = new String();
+ eesnimi = "Peedu";
+ kursus = "teisel";
+ huud = "Pets";
+ int vanus = 17;
+ int pikkus = 180;
+ int minuArv = ;
+ 
+ System.out.println("Minu nimi on "+eesnimi+" ja ma käin "+kursus+" kursusel. "+huud+" on mu hüüdnimi, ma olen "+vanus+" ja "+pikkus+" cm pikk.");
+ 
+ }
+
+}
+

0d53aeb57bd378a4f818f62584966df022d18968
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2022-09-21 12:53:17 +0300

    Add files via upload

---
+++ harjutus_05.java
@@ -0,0 +1,37 @@
+package ül;
+
+import java.util.Scanner;
+
+public class harjutus_05{
+	
+	public static void main(String[] args) {
+	
+		jagamine(0);
+	}
+	
+	public static void jagamine(int i) {
+	int k = 0;
+		
+	while (k > 3); {
+	Scanner scanner= new Scanner (System.in);
+	System.out.println("Sisesta arv: ");
+	int arv1 = scanner.nextInt();
+	System.out.println("Sisesta arv: ");
+	int arv2 = scanner.nextInt();
+
+	if (arv1 >= 0 && arv2 > 0) {
+		double vastus = arv1 / arv2;
+		System.out.println("Vastus on "+vastus);
+		k++;
+	}
+	else {
+		System.out.println("Negatiivseid arve ei tohi kasutada");
+	
+	}
+	
+		
+	}
+
+}
+}
+

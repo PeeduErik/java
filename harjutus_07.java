c4bc927f408f39c85788aab8b0debcb4dfce5b3d
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2022-09-22 13:39:57 +0300

    Add files via upload

---
+++ harjutus_07.java
@@ -0,0 +1,27 @@
+package ül;
+import java.util.Scanner;
+public class harjutus_07 {
+
+	public static void main(String[] args) {
+		viga();
+	}
+	public static void viga() {
+	
+			Scanner scanner = new Scanner (System.in); 
+		try {
+			System.out.println("Sisesta arv1:");
+			int arv1 = scanner.nextInt();
+			System.out.println("Sisesta arv2:");
+			int arv2 = scanner.nextInt();
+			
+			int s = arv1/arv2;
+			System.out.println(s);
+		} catch (Exception e) { 
+			System.out.println("Viga");
+		}
+		System.out.println("Programmi too jatkub");
+		viga();
+		   
+		}	 
+		
+	}

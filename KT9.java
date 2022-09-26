e184b81f3c7411d1ed41a0d26d46c3ce7d836727
PeeduErik <98737136+PeeduErik@users.noreply.github.com>
2022-09-26 21:14:02 +0300

    Update KT9.java

--- KT9.java
+++ KT9.java
@@ -4,7 +4,7 @@ package ül;
 public class KT9 {
 	public static void main( String args[] )   
 	{  
-		for(int i = 1; i <= 10; i++)
+		for(int i = 1; i <= 66; i++)
         {
             System.out.print(i);
         }
